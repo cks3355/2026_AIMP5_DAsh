57407d86183f7b451b240b496720db660be9c5f4
DESKTOP-ACQ5M1H\User <cks3355@naver.com>
2026-04-13 21:36:21 +0900

    test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import os
-import streamlit as st
-from langchain_core.messages.chat import ChatMessage
-from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_openai import ChatOpenAI
-from langchain_teddynote import logging
+# import os
+# import streamlit as st
+# from langchain_core.messages.chat import ChatMessage
+# from langchain_core.messages import HumanMessage, SystemMessage
+# from langchain_openai import ChatOpenAI
+# from langchain_teddynote import logging
 
 
 
