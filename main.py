81c90d275fdc12fa8003320ca55bd606a07fc6f2
DESKTOP-ACQ5M1H\User <cks3355@naver.com>
2026-04-13 10:00:42 +0900

    test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Project Start!!")
+import os
+import streamlit as st
+from langchain_core.messages.chat import ChatMessage
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain_teddynote import logging
+
+
+
